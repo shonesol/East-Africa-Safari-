039409cfae38a3e94d5992f5e83f92f5bec6eaeb
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-05-04 15:53:38 +0300

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,26 @@
+<?php
+include "backend/db.php";
+
+$result = $conn->query("SELECT * FROM bookings");
+?>
+
+<h2>All Bookings - Pearl Dream Safaris</h2>
+
+<table border="1">
+<tr>
+<th>Name</th>
+<th>Email</th>
+<th>Tour</th>
+<th>Message</th>
+</tr>
+
+<?php while($row = $result->fetch_assoc()): ?>
+<tr>
+<td><?= $row['name'] ?></td>
+<td><?= $row['email'] ?></td>
+<td><?= $row['tour'] ?></td>
+<td><?= $row['message'] ?></td>
+</tr>
+<?php endwhile; ?>
+
+</table>
