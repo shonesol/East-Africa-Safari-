0adbf3bf56dd9e973d3b8ec554080c9a77b30c9f
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-05-04 15:51:52 +0300

    Update Book.php

--- Book.php
+++ Book.php
@@ -6,12 +6,27 @@ $email = $_POST['email'];
 $tour = $_POST['tour'];
 $message = $_POST['message'];
 
+// Save to database
 $sql = "INSERT INTO bookings (name, email, tour, message)
         VALUES ('$name','$email','$tour','$message')";
+$conn->query($sql);
 
-if ($conn->query($sql)) {
-    echo "Booking sent!";
-} else {
-    echo "Error";
-}
+// SEND EMAIL
+$to = "shonesol28@gmail.com"; // your email
+$subject = "New Safari Booking - Pearl Dream Safaris";
+
+$body = "
+New booking received:
+
+Name: $name
+Email: $email
+Tour: $tour
+Message: $message
+";
+
+$headers = "From: noreply@pearldreamsafaris.com";
+
+mail($to, $subject, $body, $headers);
+
+echo "Booking sent successfully!";
 ?>
