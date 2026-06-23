aacd407571e6936666d12689dcab64064cfc4517
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-06-23 16:37:53 +0300

    Delete dashboard.php

--- dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['user'])) {
-    header("Location: login.html");
-}
-?>
-
-<h1>Welcome to Pearl Dream Safaris Dashboard</h1>
-<p>Hello <?php echo $_SESSION['user']; ?></p>
