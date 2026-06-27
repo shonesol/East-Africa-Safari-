7d94031267b451d2af7e5fda029cce699694d1cd
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-06-27 11:57:15 +0300

    Update emailjs.js

--- emailjs.js
+++ emailjs.js
@@ -1,1 +1,19 @@
+(function(){
+    emailjs.init("YOUR_PUBLIC_KEY");
+})();
 
+export function sendEmail(data){
+    return emailjs.send("YOUR_SERVICE_ID","YOUR_TEMPLATE_ID",{
+        name: data.name,
+        email: data.email,
+        phone: data.phone,
+        country: data.country,
+        destination: data.destination,
+        arrival: data.arrival,
+        departure: data.departure,
+        clients: data.clients,
+        days: data.days,
+        parks: data.parks,
+        message: data.message
+    });
+}
